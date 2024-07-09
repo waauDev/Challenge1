c06ecfe86a236a54b6b1b2d3fae67cc429ff5f87
William Acuña <unknow.citizen@protonmail.com>
2024-07-08 23:07:28 -0500

    Ya Encripta y muestra

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,20 +7,26 @@ palabra=palabra.toLowerCase();
 
 console.log(palabra);
 
-if (palabra.includes("x")){
-    console.log("Si hay a")
-}else{
-    console.log("No hay")
-}
 
-claves.set("a","enter");
-claves.set("e","imes");
-claves.set("i","ai");
-claves.set("o","ober");
-claves.set("u","ufat");
 
-for (let[key, value] of claves){
-    console.log(key + " es: " +value);
+function encriptar_texto(palabra){
+    let nuevaPalabra = '';
+    for (let i = 0; i < palabra.length; i++) {
+        if (palabra[i] === 'a') {
+            nuevaPalabra += 'ai';
+        } else if (palabra[i] === 'e') {
+            nuevaPalabra += 'enter';
+        } else if (palabra[i] === 'i') {
+            nuevaPalabra += 'imes';
+        } else if (palabra[i] === 'o') {
+            nuevaPalabra += 'ober';
+        } else if (palabra[i] === 'u') {
+            nuevaPalabra += 'ufat';
+        } else {
+            nuevaPalabra += palabra[i];
+        }
+    }
+    return nuevaPalabra;
 }
 
 function asignarTexto(elemento, texto){
@@ -29,24 +35,37 @@ function asignarTexto(elemento, texto){
     titulo.innerHTML = texto;
 }
 
-function encriptar(){
+function boton_encriptar(){
     var textarea = document.getElementById('miTextArea');
 
     var texto = textarea.value;
     if (texto == ""){
-        console.log("No hay nada");
+        textarea.focus();
     }else{
         texto = texto.toLowerCase();
         console.log("Valor guardado:");
         console.log(texto);
+        var idtexto_resultado =document.getElementById('idtexto-resultado');
         document.getElementById("outgoing-imagen").style.display = "none"; 
-        document.getElementById("idtexto-resultado").style.display = "none"; 
-        asignarTexto('idtexto-otro',texto);
-        var boton = document.getElementById('idCopiar');
-        boton.removeAttribute('hidden');
-        var outgoing = document.getElementById('idOutgoing');
-        outgoing.style.gap='90%';
+        idtexto_resultado.style.display = "none"; 
+        
+        cambio_estilo();
+        var encrip = encriptar_texto(texto);
+        console.log("texto encriptado");
+        console.log(encrip);
+        asignarTexto('idtexto-otro',encrip);
+
         
     }
     
+}
+
+function cambio_estilo(){
+    var boton = document.getElementById('idCopiar');
+    boton.removeAttribute('hidden');
+    var outgoing = document.getElementById('idOutgoing');
+    var idtexto_otro = document.getElementById('idtexto-otro');
+    idtexto_otro.style.fontSize='1.5em';
+    idtexto_otro.style.textAlign='justify';
+    idtexto_otro.style.marginTop='2%';    
 }
